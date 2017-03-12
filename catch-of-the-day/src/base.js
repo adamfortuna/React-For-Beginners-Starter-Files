3c8d61b14c038e47189dd230b7c33679c7a25e96
Adam Fortuna <adam@fortuna.name>
2017-03-12 18:46:54 -0400

    Added firebase integration

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyA0L_ELCbVgvdd3ZZFG5OTuYEzFVoiJLaE",
+  authDomain: "react-catch-of-the-day-3d879.firebaseapp.com",
+  databaseURL: "https://react-catch-of-the-day-3d879.firebaseio.com"
+});
+
+export default base;
