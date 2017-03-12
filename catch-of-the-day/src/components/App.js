d7cb6416205bad2f44a743a485bb53cde0d6ca1c
Adam Fortuna <adam@fortuna.name>
2017-03-12 19:05:37 -0400

    Save order status to localstorage and load it on load

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -22,14 +22,28 @@ class App extends React.Component {
   }
 
   componentWillMount() {
+    // Runs right before the app is rendered
     this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
+
+    // Check if there is any order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+    if(localStorageRef) {
+      console.log('isOrderLoaded is loaded')
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
   }
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
   }
 
   addFish(fish) {
@@ -70,7 +84,10 @@ class App extends React.Component {
           </ul>
         </div>
 
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          params={this.props.params} />
         <Inventory loadSamples={this.loadSamples} addFish={this.addFish} />
       </div>
     )
